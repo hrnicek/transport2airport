36c14de1942c156cf26c3f244fc031c0f10be4e1
hrnicek <hrncir39@gmail.com>
2025-09-09 20:42:07 +0200

    wip

--- resources/views/components/faqs.blade.php
+++ resources/views/components/faqs.blade.php
@@ -1,22 +1,23 @@
 <section class="pt-16 pb-24">
   <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
     <div class="max-w-2xl mx-auto mb-10 text-center lg:mb-14">
-      <h4 class="text-2xl font-base md:text-4xl md:leading-tight ">
+      <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
         Odpovědi na vaše otázky
-      </h4>
-      <p class="mt-1 text-gray-600 ">
+      </h2>
+      <p class="mt-1 text-gray-600 dark:text-neutral-400">
         Odpovědi na nejčastěji kladené otázky.
       </p>
     </div>
 
     <div class="max-w-2xl mx-auto">
-      @forelse ($faqs as $faq)
-        <div id="{{ $faq->id }}" class="hs-accordion-group">
-          <div class="p-6 hs-accordion hs-accordion-active:bg-gray-100 active"
-            id="hs-basic-with-title-and-arrow-stretched-heading-one">
+      <div class="hs-accordion-group">
+        @forelse ($faqs as $faq)
+          <div
+            class="hs-accordion hs-accordion-active:bg-gray-100 rounded-xl p-6 dark:hs-accordion-active:bg-white/10 @if ($loop->first) active @endif"
+            id="hs-basic-with-title-and-arrow-stretched-heading-{{ $faq->id }}">
             <button
               class="inline-flex items-center justify-between w-full pb-3 font-semibold text-gray-800 transition rounded-lg hs-accordion-toggle group gap-x-3 md:text-lg text-start hover:text-gray-500 focus:outline-hidden focus:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400"
-              aria-expanded="true" aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-one">
+              aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-{{ $faq->id }}">
               {{ $faq->question }}
               <svg
                 class="block text-gray-600 hs-accordion-active:hidden shrink-0 size-5 group-hover:text-gray-500 dark:text-neutral-400"
@@ -31,9 +32,9 @@
                 <path d="m18 15-6-6-6 6" />
               </svg>
             </button>
-            <div id="hs-basic-with-title-and-arrow-stretched-collapse-one"
-              class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300" role="region"
-              aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-one">
+            <div id="hs-basic-with-title-and-arrow-stretched-collapse-{{ $faq->id }}"
+              class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300 @if (!$loop->first) hidden @endif"
+              aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-{{ $faq->id }}">
               <p class="text-gray-800 dark:text-neutral-200">
                 {!! $faq->answer !!}
               </p>
@@ -41,6 +42,7 @@
           </div>
         @empty
           <p>No FAQs available.</p>
-      @endforelse
+        @endforelse
+      </div>
     </div>
 </section>
