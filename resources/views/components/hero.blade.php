38b3702c333dc73cd6060c5e26c7e4ab7148e667
hrnicek <hrncir39@gmail.com>
2025-09-22 11:56:58 +0200

    fix

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,8 +1,12 @@
-<div class="relative h-[600px] max-w-7xl mx-auto">
-  <video class="object-cover absolute top-0 left-0 z-0 w-full h-full" autoplay loop muted playsinline>
+<div class="relative lg:h-[600px] max-w-7xl mx-auto">
+  <!-- Video pro desktop -->
+  <video class="hidden object-cover absolute top-0 left-0 z-0 w-full h-full md:block" autoplay loop muted playsinline>
     <source src="/videos/hero.webm" type="video/webm">
     <source src="/videos/hero.mp4" type="video/mp4">
   </video>
+  <!-- Statický obrázek pro mobil -->
+  <div class="absolute top-0 left-0 z-0 w-full h-full bg-center bg-no-repeat bg-cover md:hidden"
+    style="background-image: url('/img/page-header.webp');"></div>
   <div class="absolute top-0 left-0 z-10 w-full h-full bg-black/70"></div>
   <div class="flex relative z-20 flex-col justify-between px-4 mx-auto max-w-7xl h-full">
     <div class="pt-12">
